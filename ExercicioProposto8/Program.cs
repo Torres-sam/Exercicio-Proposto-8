cb4ed3deacd3f30ccf2c48da582284b169eda089
Samuel Torres <samuelvianatorres@gmail.com>
2025-07-31 19:28:36 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioProposto8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class ExercicioProposto8
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um numero inteiro: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("O numero é par.");
+            }
+            else
+            {
+                Console.WriteLine("O numero é impar.");
+            }
+        }
+    }
+}
